007c40b1e4911b9880081c10d10a793b58ffd747
Miguel Silva <miguel.silva.external@telefonica.com>
2020-02-13 17:02:40 +0100

    fix with seq

--- 02_sort/bubbleSort.nim
+++ 02_sort/bubbleSort.nim
@@ -1,12 +1,12 @@
 # Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.
 
 var
-  arr: array[3, int] = [5, 1, 9]
+  arr: seq[int] = @[5, 1, 9]
 
 let
   n = arr.len
 
-proc bubbleSort(arr: array, n: int): array =
+proc bubbleSort(arr: seq, n: int) =
   for i in countup(0, n):
     echo i
 
