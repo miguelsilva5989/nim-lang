149941ae2440b7e8a6e8b9ea8f278683d4709bb7
Mike <mike.5989@hotmail.com>
2020-02-14 11:56:22 +0100

    timers

--- 02_sort/bubbleSort.nim
+++ 02_sort/bubbleSort.nim
@@ -2,15 +2,25 @@
 # Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.
 
 import sequtils
-import random
+import random # https://nim-lang.org/docs/random.html
+import times
+
+let time = cpuTime()
+
+# Call randomize() once to initialize the default random number generator
+# If this is not called, the same results will occur every time the script is executed
+randomize()
 
 var
   # sq: seq[int] = @[5, 1, 9, 4, 6, 2, 1, 3, 1, 2, 4, 5, 9]
   # sq = @[5, 1, 9] # also can be inferred
   sq = toSeq(0..100) # generate 100 numbers sequence
 
+shuffle(sq) # shuffle the array
+echo "Initial array: ", sq
+
 let
-  n = sq.len
+  n = sq.len # sequence length
 
 proc swap(sq: var seq, pos: int) = # If the procedure needs to modify the argument for the caller, a var parameter can be used, hence the "var seq"
   let tmp: int = sq[pos]
@@ -18,8 +28,6 @@ proc swap(sq: var seq, pos: int) = # If the procedure needs to modify the argume
   sq[pos+1] = tmp
 
 proc bubbleSort(sq: var seq, n: int) = # If the procedure needs to modify the argument for the caller, a var parameter can be used, hence the "var seq"
-  shuffle(sq) # shuffle the array
-  echo "Initial array: ", sq
   var c: int = 0 # swap counter
   
   for i in countup(1, n):
@@ -33,3 +41,4 @@ proc bubbleSort(sq: var seq, n: int) = # If the procedure needs to modify the ar
 
 bubbleSort(sq, n)
 echo "\nFinal sort: ", sq
+echo "Time taken: ", cpuTime() - time, "s"
