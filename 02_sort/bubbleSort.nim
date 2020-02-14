9b3b82f09554be08636febe4f70ea4262c790948
Mike <mike.5989@hotmail.com>
2020-02-14 01:29:59 +0100

    bubble sort

--- 02_sort/bubbleSort.nim
+++ 02_sort/bubbleSort.nim
@@ -1,15 +1,24 @@
+# Author: Miguel Silva
 # Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.
 
 var
-  arr: seq[int] = @[5, 1, 9]
+  sq: seq[int] = @[5, 1, 9, 4, 6, 2]
+  # sq = @[5, 1, 9] # also can be inferred
 
 let
-  n = arr.len
+  n = sq.len
 
-proc bubbleSort(arr: seq, n: int) =
-  for i in countup(0, n):
-    echo i
-    for j in countup(0, n-i):
-      echo j
+proc swap(sq: var seq, pos: int) =
+  let tmp: int = sq[pos]
+  sq[pos] = sq[pos+1]
+  sq[pos+1] = tmp
 
-bubbleSort(arr, n)
+proc bubbleSort(sq: var seq, n: int) =
+  for i in countup(1, n):
+    for j in countup(0, n-i-1):
+      echo i, ",", j, " -> ", sq[j], ",", sq[j+1]
+      if sq[j] > sq[j+1]:
+        swap(sq, j)
+
+bubbleSort(sq, n)
+echo sq
